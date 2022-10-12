542a63966143adf8a53a898ebf1479be56f3e349
zhuganglie <pyrrhonianpig@gmail.com>
2022-10-12 20:37:01 +0800

    update

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,6 +1,4 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
-import remarkGfm from 'remark-gfm';
-import remarkRehype from 'remark-rehype';
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
@@ -9,7 +7,7 @@ const config = defineConfig({
 		dashes: 'oldschool'
 	},
 
-	remarkPlugins: [remarkRehype, remarkGfm],
+	remarkPlugins: [],
 	rehypePlugins: []
 });
 
