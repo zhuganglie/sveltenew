fb545a070739ad02fbbe92457926e95e391b2682
zhuganglie <pyrrhonianpig@gmail.com>
2022-10-20 21:22:39 +0800

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,19 +11,37 @@
 	<div>
 		<img src="/images/casual.webp" alt="Welcome" class="w-full h-auto"/>
 	</div>
-	<div class="text-center">
-		<p>I know why you're here.</p>
-		  <p>I know what you've been doing.</p>
-		  <p>I know why you hardly sleep, and why night after night you sit at your computer.</p>
-		  <p>You're looking for him.</p>
-		  <p>I know, because I was once looking for the same thing.</p>
-		  <p>And when he found me, he told me I wasn't really looking for him, I was looking for an answer.</p>
-		  <p>It's the question that drives us.</p>
-		  <p>It's the question that brought you here.</p>
-		  <p>You know the question, just as I did.</p>
-		  <p class="mt-10">-----------</p>
-		  
-		</div>
+	<div class="one text-3xl text-center">
+		<h1 class="text-3xl md:ml-12 mb-12">临 江 仙</h1>
+		<p class="font-bold  font-sans  text-base md:ml-12 mb-12">[明] &middot; 杨慎 </p>
+				<p class="md:ml-4">滚滚长江东逝水，</p>
+				<p class="md:ml-4">浪花淘尽英雄。</p>
+				<p class="md:ml-4">是非成败转头空。</p>
+				<p class="md:ml-4">青山依旧在，</p>
+				<p class="md:ml-8">几度夕阳红。</p>
+                
+				<p class="md:ml-4">白发渔樵江渚上，</p>
+				<p class="md:ml-4">惯看秋月春风。</p>
+				<p class="md:ml-4">一壶浊酒喜相逢。</p>
+				<p class="md:ml-4">古今多少事，</p>
+				<p class="md:ml-4">都付笑谈中。</p>
+	</div> 
 				
 </section>
 
+<style>  
+	@import url('https://fonts.googleapis.com/css2?family=Zhi+Mang+Xing&display=swap');
+		.one {  
+			font-family: 'Zhi Mang Xing', cursive;
+			margin: 0 auto;  
+		}  
+		@media (min-width: 768px){
+			.one {
+				font-family: 'Zhi Mang Xing', cursive;
+				margin: 0 auto;
+				writing-mode: vertical-rl;/*从左向右 从右向左是 writing-mode: vertical-rl;*/  
+				writing-mode: tb-rl;/*IE浏览器的从左向右 从右向左是 writing-mode: tb-rl；*/ 
+				text-align: center;
+			}
+		}
+		</style>  
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,7 +9,6 @@
   <h1>Contact</h1>
   <hr />
     <form method="POST" class="flex flex-col space-y-4 mt-8">
-      <input type="hidden" name="access_key" value="f6f4b58b-9a7c-4704-947f-7970c5508ca8">
       <input type="text" name="name" placeholder="John Smith" required class="bg-zinc-50 p-2">
       <input type="email" name="email" placeholder="johnsmith@abc.com" required class="bg-zinc-50 p-2">
   <textarea name="message" placeholder="Message" rows="10" required class="p-2 bg-zinc-50 "></textarea>
