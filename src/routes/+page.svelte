6f19ad351869f81e9edfae8ac411db7d76b891c5
zhuganglie <pyrrhonianpig@gmail.com>
2022-11-06 23:21:26 +0800

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 	</div>
 	<div class="one text-3xl text-center">
 		<h1 class="text-3xl md:ml-12 mb-12">临 江 仙</h1>
-		<p class="font-bold  font-sans  text-base md:ml-12 mb-12">[明] &middot; 杨慎 </p>
+		<p class="font-bold  font-sans  text-base md:ml-12 mb-12">『 明 』 &middot; 杨慎 </p>
 				<p class="md:ml-4">滚滚长江东逝水，</p>
 				<p class="md:ml-4">浪花淘尽英雄。</p>
 				<p class="md:ml-4">是非成败转头空。</p>
