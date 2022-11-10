f3c08bceec3d98df5bbc54a128a5f476b93df1ab
zhuganglie <pyrrhonianpig@gmail.com>
2022-11-10 20:06:29 +0800

    update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
 	import Social from './Social.svelte'
 </script>
 
-<footer class="w-full text-center flex flex-col items-center justify-center space-y-1 mb-6">
+<footer class="w-full text-center flex flex-col md:flex-row md:items-end justify-center space-y-1 md:space-y-0 md:space-x-12  py-4">
 	<Social />
 	<p>&copy; {new Date().getFullYear()} YZC <sup><small>&reg;</small></sup></p>
 </footer>
