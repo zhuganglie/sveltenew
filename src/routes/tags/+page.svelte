19045814f9eeb62c7dcf05df97b7692e46ff5382
zhuganglie <pyrrhonianpig@gmail.com>
2023-01-31 23:58:22 +0800

    update

--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -17,12 +17,14 @@
   
   <div class="mt-8 list-none grid place-items-center place-content-center grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-2">
     {#each data.uniqueTags as tag}
+    
     <div class="rounded bg-zinc-100 px-2 py-1  min-w-max">
       <a href="/tags/{tag.title}">
         { tag.title }
       </a>
       ({tag.count})
     </div>
+    
     {/each}
   </div>
 </div>
