ff3cdde711003f014bde2449c53d7f69b01a33f8
zhuganglie <pyrrhonianpig@gmail.com>
2023-03-10 23:36:04 +0800

    update

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,3 @@
-import { start_mongo } from "./db/mongo";
-start_mongo().then(() => {
-	console.log('Mongo started');
-}).catch(e => {console.error(e)})
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = async ({ event, resolve }) => {
