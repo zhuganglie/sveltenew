29f4acc35b00095b1a3e1b4578b71eb1eecc264d
zhuganglie <pyrrhonianpig@gmail.com>
2023-03-10 23:28:25 +0800

    update

--- src/routes/about/+page.server.js
+++ src/routes/about/+page.server.js
@@ -1,19 +1,2 @@
 
-import { recipe } from "../../db/recipe";
-
-/** @type {import('./$types').PageServerLoad} */
-
-export async function load () {
-	const recArr = await recipe.find().toArray();
-	let data = recArr.map((doc) => {
-		return {...doc, _id:doc._id.toHexString()}
-	})
-	return {
-		recipe: data
-	}
-}
-
-
-
-
-//export const prerender = true;
+export const prerender = true;
