ea2aec0196933c0aa49b24661800718c76c31ced
zhuganglie <pyrrhonianpig@gmail.com>
2024-08-01 15:07:42 +0800

    update

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -37,13 +37,13 @@
 </article>
   
 <div class="flex justify-between mt-8">
-<div class="flex items-center justify-start space-x-4">
+<div class="w-fit inline-block space-x-4">
   {#each data.post.tags as tag}
-   <span class="list-none bg-zinc-200 rounded  px-2 py-0.5"> <a href="/tags/{tag}">{tag}</a></span>
+   <span class="w-fit inline-block list-none bg-zinc-200 rounded  px-2 py-0.5 space-y-2"> <a href="/tags/{tag}" class="w-fit inline-block">{tag}</a></span>
   {/each}
 </div>
-<div class="px-2 py-0.5 rounded flex items-center bg-zinc-200  max-w-max">
-    <a href={`/blog`} class="">
+<div class="px-2 py-0.5 rounded inline-block items-center bg-zinc-200  w-fit">
+    <a href={`/blog`} class="w-fit inline-block">
       All Posts 
     </a>
   </div>
